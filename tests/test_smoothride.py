0733f1c4d67f158cddd916a0a892e532c76af226
Nicholaus E. Halecky <nehalecky@gmail.com>
2013-04-03 22:12:24 -0700

    TST: Better test coverage of updated _sequentialize()
    Also, updated data in `tests/data_record_small.cs`v to include second
    intervals that don't contain all samples (approximation of data loggin system
    slowdown).

--- tests/test_smoothride.py
+++ tests/test_smoothride.py
@@ -50,7 +50,29 @@ class TestCoreMethods(unittest.TestCase):
     def test_sequentialize(self):
         df = sr._sequentialize(_df, 20, '50L')
         result = str(df.index[-1] - df.index[0])
-        expected = '0:00:23.950000'
+        expected = '0:00:22.950000'
+        self.assertEqual(result, expected)
+
+        isSeries = isinstance(_df.ix['2013-02-18 18:24:14'], pd.Series)
+        self.assertFalse(isSeries)
+
+        isSeries = isinstance(df.ix['2013-02-18 18:24:14'], pd.Series)
+        self.assertTrue(isSeries)
+
+        expected = {'RotationX': -0.021989990000000001,
+                    'RotationY': 0.13913789999999998,
+                    'RotationZ': -0.031529429999999997,
+                    'accelerationX': -0.11575319999999999,
+                    'accelerationY': -0.053253170000000002,
+                    'accelerationZ': -1.015533,
+                    'alt': 1466.0,
+                    'course': 76.289059999999992,
+                    'horizontalAccuracy': 5.0,
+                    'lat': 39.51567,
+                    'long': -119.8884,
+                    'speed': 28.539179999999998,
+                    'verticalAccuracy': 6.0}
+        result = df.ix['2013-02-18 18:24:01'].to_dict()
         self.assertEqual(result, expected)
 
 
